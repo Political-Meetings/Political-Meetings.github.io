46b8c7a2036d7c57c5703107c0e1340026c5c01b
xibalbam <maelporret@outlook.fr>
2024-07-29 18:45:07 +0200

    feat: Make logo return to home

--- src/jsMain/kotlin/components/Header.kt
+++ src/jsMain/kotlin/components/Header.kt
@@ -10,6 +10,7 @@ import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.core.rememberPageContext
 import com.varabyte.kobweb.silk.components.forms.Button
+import com.varabyte.kobweb.silk.components.navigation.Link
 import com.varabyte.kobweb.silk.theme.colors.ColorMode
 import fr.xibalba.politicalMeetings.SecondaryButton
 import fr.xibalba.politicalMeetings.utils.unaryPlus
@@ -23,7 +24,9 @@ fun Header(routes: Set<Pair<String, String>>) {
     var theme by ColorMode.currentState
     val ctx = rememberPageContext()
     Row(Modifier.position(Position.Sticky).width(100.percent), verticalAlignment = Alignment.CenterVertically) {
-        Logo(244 to 227)
+        Link("/") {
+            Logo(244 to 227)
+        }
         Column(Modifier.width(100.percent)) {
             H1(Modifier.fontSize(80.px).margin(topBottom = 10.px).toAttrs()) {
                 +"Political Meetings"
